35b8b937a7bcac026d62ee9aeca44ff960d1403f
iam26509397 <iam26509397@i03.informatica.escoladeltreball.org>
2018-10-18 20:58:56 +0200

    DOM

---
+++ DOM bibliotecas/bibliotecas/BibliotecasReader.java
@@ -0,0 +1,48 @@
+package bibliotecas;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+/**
+ * 
+ * Copyright 2015 Daniel Cruz <i>
+ * <a href="mailto:dani.cruz.morales@gmail.com">dani.cruz.morales@gmail.com</a><br><br>
+ * 
+ * This is free software, licensed under the GNU General Public License v3.<br>
+ * See <a href="http://www.gnu.org/licenses/gpl.html">http://www.gnu.org/licenses/gpl.html</a> for more information.
+ *
+ * @author Daniel Cruz 
+ *
+ */
+public class BibliotecasReader extends DomReader {
+
+	public BibliotecasReader(String xml) throws ParserConfigurationException, SAXException, IOException {
+		super(xml);
+		
+	}
+	
+	
+	public List<String> listarMailsPorCP(String cp){
+		return(super.extractList("//row[librarycp="+cp+"]/libraryemail/text()"));
+	}
+	
+	public String telefonoPorID(String id){
+		return(super.extractValue("//row[@num="+id+"]/libraryphone/text()"));
+	}
+
+
+	public String idPorTelefono(String telf) {
+		
+		return(super.extractValue("//row[libraryphone='"+telf+"']/@num"));
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ DOM bibliotecas/bibliotecas/MainBibliotecas.java
@@ -0,0 +1,50 @@
+package bibliotecas;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+/**
+ * 
+ * Copyright 2015 Daniel Cruz <i>
+ * <a href="mailto:dani.cruz.morales@gmail.com">dani.cruz.morales@gmail.com</a><br><br>
+ * 
+ * This is free software, licensed under the GNU General Public License v3.<br>
+ * See <a href="http://www.gnu.org/licenses/gpl.html">http://www.gnu.org/licenses/gpl.html</a> for more information.
+ *
+ * @author Daniel Cruz 
+ *
+ */
+public class MainBibliotecas {
+
+	
+
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+		
+		BibliotecasReader r=new BibliotecasReader("data/bibliotecas.xml");
+		
+		
+		String cp="48450";
+		System.out.println("emails para cp="+cp);
+		for(String s:r.listarMailsPorCP(cp)){
+			System.out.println("\t"+s);
+		}
+		System.out.println();
+		
+		String id="6";
+		System.out.println("Telefono para id (row) "+id);
+		System.out.println("\t"+r.telefonoPorID(id));
+		System.out.println();
+		
+		String telf="944 672 946";
+		System.out.println("Id para telefono "+telf);
+		System.out.println("\t"+r.idPorTelefono(telf));
+
+
+	}
+
+	
+
+}
